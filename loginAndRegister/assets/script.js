9929133cc42e6d68d66b26060083cb751402e002
devJhn <contatojohn221@gmail.com>
2022-10-13 15:59:42 -0300

    v1

---
+++ loginAndRegister/assets/script.js
@@ -0,0 +1,15 @@
+var x = document.getElementById('entrar');
+var y = document.getElementById('registro');
+var z = document.getElementById('btn');
+
+function registro(){
+x.style.left = "-400px";
+y.style.left = "50px";
+z.style.left = "110px";
+}
+
+function entrar(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
